cc992d5d9eb60415f38e03d4d24fcc403b6482b9
Krishan Kumar <krishan97@users.noreply.github.com>
2020-10-12 09:10:50 +0530

    init

---
+++ src/constant/index.ts
@@ -0,0 +1,9 @@
+
+export const PORT = 4000;
+export const JWT_SECRET = 'V1ZST1MyTkhUWGxoUjJocFlWUkdlRnBXWXpWTlIwWlVUVVJTVGxKR1ZYZFVWbEpXWldzeE5sSlVWVDA9';
+export const SERVER_URL = `localhost:${PORT}`;
+export const SWAGGER_APP_NAME  = 'Sample';
+export const SWAGGER_VERSION  = '1.0.0';
+export const SWAGGER_DESC  = 'RESTful API';
+export const SWAGGER_CONTACT_MAIL  = 'krishan.6297@gmail.com';
+export const COMPANY_NAME  = '';
